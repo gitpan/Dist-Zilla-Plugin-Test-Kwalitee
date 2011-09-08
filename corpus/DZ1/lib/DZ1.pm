a55d4a79df1f1f10fd7133ac24435af8ad77edee
Alan Young <harleypig@gmail.com>
2011-09-08 02:48:14 +0000

    Import of AYOUNG/Dist-Zilla-Plugin-Test-Kwalitee-2.020000 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-Plugin-Test-Kwalitee
    gitpan-cpan-version:      2.020000
    gitpan-cpan-path:         AYOUNG/Dist-Zilla-Plugin-Test-Kwalitee-2.020000.tar.gz
    gitpan-cpan-author:       AYOUNG
    gitpan-cpan-maturity:     released

--- corpus/DZ1/lib/DZ1.pm
+++ corpus/DZ1/lib/DZ1.pm
@@ -1,6 +1,8 @@
 use strict;
 use warnings;
+
 package DZ1;
+
 # ABSTRACT: this is a sample package for testing Dist::Zilla;
 
 sub main {
--- lib/Dist/Zilla/Plugin/KwaliteeTests.pm
+++ lib/Dist/Zilla/Plugin/KwaliteeTests.pm
@@ -3,15 +3,19 @@ use strict;
 use warnings;
 
 package Dist::Zilla::Plugin::KwaliteeTests;
+{
+  $Dist::Zilla::Plugin::KwaliteeTests::VERSION = '2.020000';
+}
+
 # ABSTRACT: (DEPRECATED) Release tests for kwalitee
-our $VERSION = '2.0.1'; # VERSION
 use Moose;
 extends 'Dist::Zilla::Plugin::Test::Kwalitee';
 
 
 before register_component => sub {
-    warn '!!! [KwaliteeTests] is deprecated and will be removed in a future release; replace it with [Test::Kwalitee]';
+  warn '!!! [KwaliteeTests] is deprecated, and will be removed in a future release. Please use [Test::Kwalitee] instead.';
 };
+
 __PACKAGE__->meta->make_immutable;
 no Moose;
 1;
@@ -19,51 +23,32 @@ no Moose;
 __END__
 =pod
 
-=encoding utf-8
-
 =head1 NAME
 
 Dist::Zilla::Plugin::KwaliteeTests - (DEPRECATED) Release tests for kwalitee
 
 =head1 VERSION
 
-version 2.0.1
-
-=head1 SYNOPSIS
-
-Please use L<Dist::Zilla::Plugin::Test::Kwalitee> instead.
-
-In C<dist.ini>:
-
-    [Test::Kwalitee]
+version 2.020000
 
 =for test_synopsis 1;
 __END__
 
-=head1 AVAILABILITY
+=head1 SYNOPSIS
 
-The project homepage is L<http://metacpan.org/release/Dist-Zilla-Plugin-Test-Kwalitee/>.
+In C<dist.ini>:
 
-The latest version of this module is available from the Comprehensive Perl
-Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN
-site near you, or see L<http://search.cpan.org/dist/Dist-Zilla-Plugin-Test-Kwalitee/>.
+    [Test::Kwalitee]
+    skiptest=use_strict ; Don't test for strictness.
 
-The development version lives at L<http://github.com/doherty/Dist-Zilla-Plugin-Test-Kwalitee>
-and may be cloned from L<git://github.com/doherty/Dist-Zilla-Plugin-Test-Kwalitee.git>.
-Instead of sending patches, please fork this project using the standard
-git and github infrastructure.
+=head1 DESCRIPTION
 
-=head1 SOURCE
+Please use L<Dist::Zilla::Plugin::Test::Kwalitee> instead.
 
-The development version is on github at L<http://github.com/doherty/Dist-Zilla-Plugin-Test-Kwalitee>
-and may be cloned from L<git://github.com/doherty/Dist-Zilla-Plugin-Test-Kwalitee.git>
+This is an extension of L<Dist::Zilla::Plugin::InlineFiles>, providing the
+following file:
 
-=head1 BUGS AND LIMITATIONS
-
-No bugs have been reported.
-
-Please report any bugs or feature requests through the web interface at
-L<https://github.com/doherty/Dist-Zilla-Plugin-Test-Kwalitee/issues>.
+  xt/release/kwalitee.t - a standard Test::Kwalitee test
 
 =head1 AUTHORS
 
@@ -71,17 +56,25 @@ L<https://github.com/doherty/Dist-Zilla-Plugin-Test-Kwalitee/issues>.
 
 =item *
 
-Marcel Grünauer <marcel@cpan.org>
+Harley Pig <harleypig@gmail.com>
 
 =item *
 
-Mike Doherty <doherty@cpan.org>
+Caleb Cushing <xenoterracide@gmail.com>
+
+=item *
+
+Marcel Gruenauer <marcel@cpan.org>
+
+=item *
+
+Kent Fredric <kentfredric@gmail.com>
 
 =back
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2010 by Mike Doherty.
+This software is copyright (c) 2011 by Alan Young.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
